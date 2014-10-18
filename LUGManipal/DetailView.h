f79119a86afe05bd0046397e1034f795217cebac
luk4shubhs <luk4shubhs@gmail.com>
2014-10-19 01:29:43 +0530

    Added pull to refresh. Worked on navigation bar show/hide animation on scrolling.

---
+++ LUGManipal/DetailView.h
@@ -0,0 +1,13 @@
+//
+//  DetailView.h
+//  LUGManipal
+//
+//  Created by Shubham Sorte on 19/10/14.
+//  Copyright (c) 2014 LUG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailView : UIView
+
+@end
