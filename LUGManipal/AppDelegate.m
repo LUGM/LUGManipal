69285d7b4753f7e01f07131c7eed1236b073c6f7
luk4shubhs <luk4shubhs@gmail.com>
2014-12-13 00:09:12 +0530

    Push Service Implemented and tested

--- LUGManipal/AppDelegate.m
+++ LUGManipal/AppDelegate.m
@@ -18,7 +18,27 @@
     
     [Parse setApplicationId:@"wPBqAPFLBuSms7zJ5z3d3xWjV5VBMCXQNwpBaJRO" clientKey:@"4GjHWzJlDMNp8s2yvP8U5PCjkhVNIMrV6QfI8S1O"];
     
+    UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                    UIUserNotificationTypeBadge |
+                                                    UIUserNotificationTypeSound);
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
+                                                                             categories:nil];
+    [application registerUserNotificationSettings:settings];
+    [application registerForRemoteNotifications];
+    
     return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    currentInstallation.channels = @[ @"global" ];
+    [currentInstallation saveInBackground];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [PFPush handlePush:userInfo];
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
